b4e3773dff64257adef0d1680a49b0609e1d9db3
sharmanodejs <128783088+sharmanodejs@users.noreply.github.com>
2023-09-02 23:47:09 +0530

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Logo = (props: Props) => {
     return (
         <Link to={'/'} className="logo text-3xl font-semibold text-white">
-            Movie<span className='text-dark-teal'>Flix</span>
+            FreeHindi<span className='text-dark-teal'>MoviesHd</span>
         </Link>
     )
 }
